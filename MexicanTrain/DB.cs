6a6f73ace919642adfd5fea3b505c076755c4e78
Dcyclone1 <153251085+Dcyclone1@users.noreply.github.com>
2023-12-07 21:47:05 -0500

    Update DB.cs
    Added this code. Method DateList

--- MexicanTrain/DB.cs
+++ MexicanTrain/DB.cs
@@ -43,6 +43,30 @@ namespace MexicanTrain
             cnn.Close();
             return playerList;
 
+             public static List<string> DateList()
+        {
+            SqlConnection cnn;
+            cnn = new SqlConnection(CnnHelper.CnnVal("GameMasters3"));
+            cnn.Open();
+            List<string> game_date = new List<string>();
+
+            string dateListQuery = "select game_session from GameSessions";
+            SqlCommand cmd = new SqlCommand(dateListQuery, cnn);
+            SqlDataReader reader= cmd.ExecuteReader();
+            while (dataReader.Read())
+            {
+                try
+                {
+                    game_dateList.Add((string)dataReader.GetValue(0));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                cnn.Close();
+                return game_date;
+            }
+
         }
 
         //public List<MTGame> GetGames()
